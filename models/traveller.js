74f4a47b4661ebb33558e4c0c1bc0424d4a2f79b
StuMcA <stuart.mcara@outlook.com>
2021-03-31 19:13:22 +0100

    finished hw

--- models/traveller.js
+++ models/traveller.js
@@ -3,26 +3,39 @@ const Traveller = function(journeys) {
 };
 
 Traveller.prototype.getJourneyStartLocations = function() {
-
+  return this.journeys.map(journey => {
+    return journey.startLocation
+  });
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
-
+  return this.journeys.map(journey => {
+    return journey.endLocation
+  })
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-
+  return this.journeys.filter(journey => journey.transport === transport)
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-
+  return this.journeys.filter(journey => journey.distance >= minDistance)
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-
+  return this.journeys.reduce((accumulator, journey) => {
+    return accumulator + journey.distance;
+  }, 0)
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
+  let modesOfTransport = [];
+  this.journeys.forEach((journey) => {
+    if (!modesOfTransport.includes(journey.transport)) {
+      modesOfTransport.push(journey.transport);
+    }
+  })
+  return modesOfTransport;
 
 };
 
